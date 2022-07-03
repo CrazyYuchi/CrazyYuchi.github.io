96b2435b2a9814f9e9b2ac179ab7694bb5ffec91
CrazyYuchi <931846427@qq.com>
2022-07-03 11:18:46 +0800

    Site updated: 2022-07-03 11:18:17

---
+++ videos/js/Resource.js
@@ -0,0 +1,20 @@
+data=
+[
+    {"xinshang_好看的小姐姐_001_汪小敏-笑看风云_p_.jpg": "https://pic.rmb.bdstatic.com/bjh/b9605ecbd64a0b6dacfece1b8a24ab9c.png" },
+    {"xinshang_好看的小姐姐_001_汪小敏-笑看风云_v_.mp4": "https://upos-sz-mirrorcos.bilivideo.com/upgcxcode/76/31/299443176/299443176-1-208.mp4?e=ig8euxZM2rNcNbhjnWdVhwdlhzTHhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1656782893&gen=playurlv2&os=cosbv&oi=1848246899&trid=a96f75ccf36242a9b8df30cb491aeb64T&mid=0&platform=html5&upsig=bd22967c32c3e487d3fad3fa37e8326d&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,mid,platform&bvc=vod&nettype=0&bw=371245&orderid=0,1&logo=80000000_v_.mp4" },
+    /*{"xinshang_好看的小姐姐_002_转发哔站一支陶然_v_.mp4":"https://upos-sz-mirrorcos.bilivideo.com/upgcxcode/08/36/731363608/731363608-1-208.mp4?e=ig8euxZM2rNcNbhBhwdVhwdlhzUVhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1656781401&gen=playurlv2&os=alibv&oi=1848246899&trid=e68b1274c6d5451db4eb3251817794dcT&mid=0&platform=html5&upsig=970abb2583f2d142ea7293c209238805&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,mid,platform&bvc=vod&nettype=0&bw=350235&orderid=0,1&logo=80000000_v_.mp4"},
+    */
+
+    /*{"jingse_美丽的风景_001_暴雪星际争霸_p_.jpg":"https://pic.rmb.bdstatic.com/bjh/fccc9e8a09eec97b602dfa4d3b642b4e.png"},
+    {"jingse_美丽的风景_001_美丽中国_v_.mp4":"http://985.so/c1h1_v_.mp4"},*/
+
+    {"wanshua_游戏谁不爱玩_001_暴雪星际争霸_p_.jpg":"https://pic.rmb.bdstatic.com/bjh/fccc9e8a09eec97b602dfa4d3b642b4e.png"},
+    {"wanshua_游戏谁不爱玩_001_暴雪星际争霸_v_.mp4":"https://resource-1308065039.cos.ap-chengdu.myqcloud.com/SC2_SC2_001_CG1_v_.mp4"},
+    /*{"wanshua_游戏谁不爱玩_002_炉石传说_v_.mp4":"_v_.mp4"},*/   
+
+    {"shuijiao_睡觉别睁眼_001_掏耳朵_p_.jpg":"https://pic.rmb.bdstatic.com/bjh/b3b0ecf08154b71e6e8af32dc487318f.png"},
+    {"shuijiao_睡觉别睁眼_001_掏耳朵_v_.mp4":"https://upos-sz-mirrorcos.bilivideo.com/upgcxcode/85/12/82751285/82751285-1-192.mp4?e=ig8euxZM2rNcNbRVhwdVhwdlhWdVhwdVhoNvNC8BqJIzNbfq9rVEuxTEnE8L5F6VnEsSTx0vkX8fqJeYTj_lta53NCM=&uipk=5&nbs=1&deadline=1656824848&gen=playurlv2&os=cosbv&oi=1848246899&trid=2ff38ae3dae048f394c488af20c8ec61T&mid=0&platform=html5&upsig=c2dd50b231876224334bd04997469020&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,mid,platform&bvc=vod&nettype=0&bw=69051&orderid=0,1&logo=80000000_v_.mp4"}
+
+
+]
+
